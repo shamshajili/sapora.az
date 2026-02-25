73367a26e6d0c82e7563c4086d2263b17fded01a
shamshajili <shamshacili@gmail.com>
2026-02-25 23:05:01 +0400

    added footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/images/saporalogo.png";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="bg-[#123a3d] text-white">
+      <div className="max-w-5xl mx-auto px-6 py-24 text-center">
+        <img
+          src={logo}
+          alt="Sapora Logo"
+          className="w-72 mx-auto mb-10"
+        />
 
-export default Footer
+        <a
+          href="https://www.instagram.com/sapora.az/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="
+            inline-block
+            px-10 py-3
+            border border-[#c9a46a]
+            text-[#c9a46a]
+            text-sm tracking-wide
+            transition
+            hover:bg-[#c9a46a]
+            hover:text-[#123a3d]
+          "
+        >
+          Follow us on Instagram
+        </a>
+      </div>
+
+      <div className="border-t border-white/20" />
+
+      <div className="text-center py-6 text-sm opacity-80">
+        © {new Date().getFullYear()} Sapora. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/common/Hero";
 import Menu from "../components/menu/Menu";
+import Footer from "../components/common/Footer";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Menu />
+      <Footer/>
     </div>
   );
 };
