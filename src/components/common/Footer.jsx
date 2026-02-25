9f973fa38dc700a411543e496bf2c7d148c02754
shamshajili <shamshacili@gmail.com>
2026-02-25 23:18:49 +0400

    added footer fix cssx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,19 +4,23 @@ import logo from "../../assets/images/saporalogo.png";
 const Footer = () => {
   return (
     <footer className="bg-[#123a3d] text-white">
-      <div className="max-w-5xl mx-auto px-6 py-24 text-center">
+      <div className="max-w-5xl mx-auto px-6 py-20 flex flex-col items-center gap-6">
+        {/* LOGO – OPTICAL CENTER */}
         <img
           src={logo}
           alt="Sapora Logo"
-          className="w-72 mx-auto mb-10 mr-1"
+          className="w-72 translate-x-[3px]"
         />
 
+        {/* BUTTON – OPTICAL CENTER */}
         <a
           href="https://www.instagram.com/sapora.az/"
           target="_blank"
           rel="noopener noreferrer"
           className="
-            inline-block
+            inline-flex
+            items-center
+            justify-center
             px-8 py-2
             border border-[#c9a46a]
             text-[#c9a46a]
@@ -24,8 +28,7 @@ const Footer = () => {
             transition
             hover:bg-[#c9a46a]
             hover:text-[#123a3d]
-            mt-[-12px]
-            ml-2
+            -translate-x-[2px]
           "
         >
           Follow us on Instagram
@@ -34,7 +37,7 @@ const Footer = () => {
 
       <div className="border-t border-white/20" />
 
-      <div className="text-center py-6 text-sm opacity-80">
+      <div className="text-center py-5 text-sm opacity-80">
         © {new Date().getFullYear()} Sapora. All rights reserved.
       </div>
     </footer>
