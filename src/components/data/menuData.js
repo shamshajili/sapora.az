369c673ed93b2a21eb4349b4c59504a4bc4ac24e
shamshajili <shamshacili@gmail.com>
2026-06-11 19:17:14 +0400

    added new item

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,116 +1,99 @@
-import noodle1 from "../../assets/images/pasta1.jpeg";
-import noodle2 from "../../assets/images/pasta2.jpeg";
-import noodle3 from "../../assets/images/pasta3.jpeg";
-import sorba1 from "../../assets/images/lentilsoup.jpeg";   
-import sorba2 from "../../assets/images//brocolisoup.jpeg";
-import sorba3 from "../../assets/images/mashroomsoup.jpeg";
-import sorba4 from "../../assets/images/tomatosoup.jpeg";
-import legion from "../../assets/images/legion.jpeg";
-import margherita from "../../assets/images/margherita.jpeg"; 
-import bbq from "../../assets/images/bbq.jpeg";
-import chickkenmashroom from "../../assets/images/chickenmashroom.jpeg";
-import dimare from "../../assets/images/dimare.jpeg";
-import vegetables from "../../assets/images/vegetables.jpeg";
-
-export const menuData = {
-     noodles: [
-  {
-    id: 1,
-    title: "Asian Shrimp Noodles",
-    description: "Karides, noodle, rəngli bibər, soya sousu, yaşıl soğan",
-    price: 27,
-    image: noodle2,
-  },
-  {
-    id: 2,
-    title: "Chicken Teriyaki Noodles",
-    description: "Toyuq filesi, noodle, tərəvəzlər, teriyaki və soya sousu",
-    price: 20,
-    image: noodle1,
-  },
-  {
-    id: 3,
-    title: "Salmon Wok Noodles",
-    description: "Qızıl balıq, noodle, tərəvəzlər, soya sousu, göyərti",
-    price: 24,
-    image: noodle3,
-  },
+noodles: [
+{
+id: 1,
+title: "Karidesli Wok Noodle",
+description: "Karides, yumurta noodle, rəngli bibər, soya sousu və yaşıl soğan",
+price: 27,
+image: noodle2,
+},
+{
+id: 2,
+title: "Toyuqlu Teriyaki Noodle",
+description: "Toyuq filesi, noodle, tərəvəzlər, teriyaki sousu və soya sousu",
+price: 20,
+image: noodle1,
+},
+{
+id: 3,
+title: "Qızıl Balıqlı Wok Noodle",
+description: "Qızıl balıq, noodle, tərəvəzlər, soya sousu və təzə göyərti",
+price: 24,
+image: noodle3,
+},
 ],
-  sorbalar: [
-  {
-    id: 1,
-    title: "Mərci Şorbası",
-    description: "Qırmızı mərcimək, kök, soğan, ədviyyatlar",
-    price: 8,
-    image: sorba1,
-  },
-  {
-    id: 2,
-    title: "Krem Brokoli Şorbası",
-    description: "Brokoli, qaymaq, süd, xüsusi ədviyyatlar",
-    price: 10,
-    image: sorba2,
-  },
-  {
-    id: 3,
-    title: "Krem Göbələk Şorbası",
-    description: "Təzə göbələk, qaymaq, süd, kərə yağı",
-    price: 12,
-    image: sorba3,
-  },
-  {
-    id: 4,
-    title: "Pomidor Şorbası",
-    description: "Pomidor, reyhan, sarımsaq, xüsusi sous",
-    price: 10,
-    image: sorba4,
-  },
-  ],
 
- 
+sorbalar: [
+{
+id: 1,
+title: "Mərci Şorbası",
+description: "Qırmızı mərcimək, kök, soğan və xüsusi ədviyyatlar",
+price: 8,
+image: sorba1,
+},
+{
+id: 2,
+title: "Kremli Brokoli Şorbası",
+description: "Brokoli, qaymaq, süd və xüsusi ədviyyatlar",
+price: 10,
+image: sorba2,
+},
+{
+id: 3,
+title: "Kremli Göbələk Şorbası",
+description: "Təzə göbələk, qaymaq, süd və kərə yağı",
+price: 12,
+image: sorba3,
+},
+{
+id: 4,
+title: "Pomidor Şorbası",
+description: "Pomidor, reyhan, sarımsaq və xüsusi sous",
+price: 10,
+image: sorba4,
+},
+],
 
-  Pizza: [
-  {
-    id: 1,
-    title: "Legion Pizza",
-    description: "Mozzarella, pepperoni, göbələk, zeytun",
-    price: 24,
-    image: legion,
-  },
-  {
-    id: 2,
-    title: "Margherita Pizza",
-    description: "Mozzarella, pomidor sousu, reyhan",
-    price: 22,
-    image: margherita,
-  },
-  {
-    id: 3,
-    title: "BBQ Sapora Pizza",
-    description: "BBQ sousu, toyuq əti, mozzarella, qırmızı soğan",
-    price: 26,
-    image: bbq,
-  },
-  {
-    id: 4,
-    title: "Chicken Mushroom Pizza",
-    description: "Toyuq əti, göbələk, mozzarella, qaymaqlı sous",
-    price: 25,
-    image: chickkenmashroom,
-  },
-  {
-    id: 5,
-    title: "Di Mare Pizza",
-    description: "Karides, kalmar, mozzarella, dəniz məhsulları",
-    price: 30,
-    image: dimare,
-  },
-  {
-    id: 6,
-    title: "Vegetable Pizza",
-    description: "Bibər, göbələk, qarğıdalı, zeytun, mozzarella",
-    price: 21,
-    image: vegetables,
-  },
+Pizza: [
+{
+id: 1,
+title: "Legion Pizza",
+description: "Mozzarella pendiri, pepperoni, göbələk və zeytun",
+price: 24,
+image: legion,
+},
+{
+id: 2,
+title: "Margherita Pizza",
+description: "Mozzarella pendiri, pomidor sousu və təzə reyhan",
+price: 22,
+image: margherita,
+},
+{
+id: 3,
+title: "BBQ Sapora Pizza",
+description: "BBQ sousu, toyuq əti, mozzarella pendiri və qırmızı soğan",
+price: 26,
+image: bbq,
+},
+{
+id: 4,
+title: "Toyuqlu Göbələk Pizza",
+description: "Toyuq əti, göbələk, mozzarella pendiri və qaymaqlı sous",
+price: 25,
+image: chickkenmashroom,
+},
+{
+id: 5,
+title: "Di Mare Pizza",
+description: "Karides, kalmar, mozzarella pendiri və dəniz məhsulları",
+price: 30,
+image: dimare,
+},
+{
+id: 6,
+title: "Tərəvəzli Pizza",
+description: "Bibər, göbələk, qarğıdalı, zeytun və mozzarella pendiri",
+price: 21,
+image: vegetables,
+},
 ]
-};
